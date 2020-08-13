e86a89e821f61fea1d7ffb3df33663c82aadfef6
Alex <test@mail.ru>
2020-08-13 14:49:09 +0300

    first

---
+++ StdLib_CAN/app/main.c
@@ -0,0 +1,26 @@
+//*****************************************************************************
+//
+//! \file main.c
+//! \brief main application
+//! \version 1.0.0.0
+//! \date $Creat_time$
+//! \author $Creat_author$
+//! \copy
+//!
+//! Copyright (c) 2014 CooCox.  All rights reserved.
+//
+//! \addtogroup project
+//! @{
+//! \addtogroup main
+//! @{
+//*****************************************************************************
+
+int main(void)
+{
+
+    while(1)
+    {
+    }
+}
+//! @}
+//! @}
